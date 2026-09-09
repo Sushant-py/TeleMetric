cf58440d36822fadcf76705bdd3c3071871e4b0d
Sushant-py <sushantsinha1007@gmail.com>
2026-09-09 23:27:05 +0530

    Add home.py dashboard and update router 3

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -3,12 +3,13 @@ from pathlib import Path
 
 # Automatically find the exact path to the pages folder
 PAGES_DIR = Path(__file__).parent / "pages"
-# 1. Define the pages
-home_page = st.Page("app/pages/home.py", title="TeleMetric", icon="🚀")
-explorer_page = st.Page("app/pages/01_Customer_Explorer.py", title="Customer Explorer")
-risk_page = st.Page("app/pages/02_Risk_Intelligence.py", title="Risk Intelligence")
-simulator_page = st.Page("app/pages/03_Retention_Simulator.py", title="Retention Simulator")
-insights_page = st.Page("app/pages/04_Model_Insights.py", title="Model Insights")
+
+# 1. Define the pages using the foolproof paths
+home_page = st.Page(str(PAGES_DIR / "home.py"), title="TeleMetric", icon="🚀")
+explorer_page = st.Page(str(PAGES_DIR / "01_Customer_Explorer.py"), title="Customer Explorer")
+risk_page = st.Page(str(PAGES_DIR / "02_Risk_Intelligence.py"), title="Risk Intelligence")
+simulator_page = st.Page(str(PAGES_DIR / "03_Retention_Simulator.py"), title="Retention Simulator")
+insights_page = st.Page(str(PAGES_DIR / "04_Model_Insights.py"), title="Model Insights")
 
 # 2. Pass the defined variables into the navigation router
 pg = st.navigation([
